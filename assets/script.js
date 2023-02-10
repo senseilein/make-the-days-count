e75b16c91053b76f3395dbf091c162ecb4d8e1c2
Samir Trivedi <samtrivedi18@gmail.com>
2023-02-10 20:07:08 +0000

    implement giphy api and confirmed data display

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const giphyURL = "https://api.giphy.com/v1/gifs/random?api_key=JH2woZDs5AK3mRK0PqE52IEvHoYHhQ84";
+$.ajax({
+    url: giphyURL,
+    method: "GET"
+}).then(
+    function (response) {
+        //console.log(response.data.images.original_mp4.mp4);
+        showGiphy(response);
+    }
+
+)
+
+function showGiphy(response) {
+    const giphy = $("#giphy");
+    const imageEl = $("<img>");
+    imageEl.attr("src", response.data.images.original.url);
+    giphy.append(imageEl);
+
+}
+
+
